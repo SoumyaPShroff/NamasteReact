3d5fe7e7da6e4748f53aaabadbb8dd62bef37b70
SoumyaPShroff <bsoumyashri@gmail.com>
2025-06-04 19:18:19 +0530

    Test04

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ const jsxheading = <h1 id="heading">Namaste React using JSX</h1>;
  const HeadingComponent = () => {
     return (
         <div id='container'>
-              <h1 className="heading">Namaste React using Function Component 3</h1>
+              <h1 className="heading">Namaste React using Function Component-Test</h1>
         </div>
       
     );
